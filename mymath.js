d49b2f7b6378109e0b9ed39e6d793caceccd213e
dmholtz <56723830+dmholtz@users.noreply.github.com>
2022-01-15 11:01:39 +0100

    Fix the bug

--- mymath.js
+++ mymath.js
@@ -3,7 +3,7 @@ const { isNumberObject } = require("util/types");
 const add = (a, b) => a + b;
 const mult = (a, b) => {
     if (isNaN(a) || isNaN(b)) {
-        return undefined; // this is a bug
+        return Number.NaN; // this is a bug
     } else {
         return a * b;
     }
