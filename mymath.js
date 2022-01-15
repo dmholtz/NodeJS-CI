db2d8351e697fb54f7782bfaee57653df20b5a8c
dmholtz <56723830+dmholtz@users.noreply.github.com>
2022-01-15 10:57:14 +0100

    Introduce a bug into mymath

--- mymath.js
+++ mymath.js
@@ -1,5 +1,13 @@
+const { isNumberObject } = require("util/types");
+
 const add = (a, b) => a + b;
-const mult = (a, b) => a * b;
+const mult = (a, b) => {
+    if (!isNumberObject(a) || !isNumberObject(b)) {
+        return undefined; // this is a bug
+    } else {
+        return a * b;
+    }
+};
 
 exports.add = add;
 exports.mult = mult;
